396c7ddfa6c7d52f9cc7b0c0a5b2c84f5c81ad26
Ganesandivya <ganesandivya0911@gmail.com>
2024-03-13 09:58:42 +0530

    add two files

---
+++ Electric_Bill.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import random
+
+class Bill_App:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("1300x700+0+0")
+        self.root.maxsize(width = 1280,height = 700)
+        self.root.minsize(width = 1280,height = 700)
+        self.root.title("Electric Bill")
+        self.cus_name = StringVar()
+        self.c_phone = StringVar()
+        x = random.randint(1000,9999)
+        self.c_bill_no = StringVar()
+        self.c_bill_no.set(str(x))
+
+        self.total_cosmetics = StringVar()
+        self.total_grocery = StringVar()
+        self.total_other = StringVar()
+        self.tax_cos = StringVar()
+        self.tax_groc = StringVar()
+        self.tax_other = StringVar()
+        bg_color = "#074463"
+        fg_color = "white"
+        lbl_color = 'white'
+        title = Label(self.root,text = "Electric Bill ",bd = 12,relief = GROOVE,fg = fg_color,bg = bg_color,font=("times new roman",30,"bold"),pady = 3).pack(fill = X)
+        F1 = LabelFrame(text = "Customer Details",font = ("time new roman",12,"bold"),fg = "gold",bg = bg_color,relief = GROOVE,bd = 10)
+        F1.place(x = 0,y = 80,relwidth = 1)
+        cname_lbl = Label(F1,text="Customer Name",bg = bg_color,fg = fg_color,font=("times new roman",15,"bold")).grid(row = 0,column = 0,padx = 10,pady = 5)
+        cname_en = Entry(F1,bd = 8,relief = GROOVE,textvariable = self.cus_name)
+        cname_en.grid(row = 0,column = 1,ipady = 4,ipadx = 30,pady = 5)
+        cphon_lbl = Label(F1,text = "Phone No",bg = bg_color,fg = fg_color,font = ("times new roman",15,"bold")).grid(row = 0,column = 2,padx = 20)
+        cphon_en = Entry(F1,bd = 8,relief = GROOVE,textvariable = self.c_phone)
+        cphon_en.grid(row = 0,column = 3,ipady = 4,ipadx = 30,pady = 5)
+        cbill_lbl = Label(F1,text = "Bill No.",bg = bg_color,fg = fg_color,font = ("times new roman",15,"bold"))
+        cbill_lbl.grid(row = 0,column = 4,padx = 20)
+        cbill_en = Entry(F1,bd = 8,relief = GROOVE,textvariable = self.c_bill_no)
+        cbill_en.grid(row = 0,column = 5,ipadx = 30,ipady = 4,pady = 5)
+        bill_btn = Button(F1,text = "Enter",bd = 7,relief = GROOVE,font = ("times new roman",12,"bold"),bg = bg_color,fg = fg_color)
+        bill_btn.grid(row = 0,column = 6,ipady = 5,padx = 60,ipadx = 19,pady = 5)
+        F2 = LabelFrame(self.root,text = 'Cosmetics',bd = 10,relief = GROOVE,bg = bg_color,fg = "gold",font = ("times new roman",13,"bold"))
+        F2.place(x = 5,y = 180,width = 325,height = 380)
